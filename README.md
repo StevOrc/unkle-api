c6fbd741e76767ea68e587e17894e3d7e6540796
Steven ORCUN <steven.orcun@niji.fr>
2022-02-24 23:30:23 +0100

    feat(unkle-0001): init porject

---
+++ README.md
@@ -0,0 +1,1 @@
+This API is an introdcution to the basis concepts of a Nodejs application using the express framework.
---
+++ src/app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// Instance of express
+const app = express();
+
+// Basic config of an express APP
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/v1/greed", (req, res) => {
+  res.status(200).send({ message: "Welcome to the app" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
