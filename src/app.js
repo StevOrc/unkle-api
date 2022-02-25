eb0a70c300c823bc702f0b58f333faddc188576a
Steven ORCUN <steven.orcun@niji.fr>
2022-02-25 15:42:18 +0100

    feat(ukle-0004): auth routes + jwt helper + validation schema impl

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const initDb = require("./helpers/db-init");
+const authRouter = require("./routes/auth.routes");
+const { errorHandling } = require("./middlewares/errorHandling");
+const createError = require("http-errors");
 
 // mongodb connection
 initDb();
@@ -18,6 +21,17 @@ app.use(cors());
 app.get("/api/v1/greed", (req, res) => {
   res.status(200).send({ message: "Welcome to the app" });
 });
+
+// Routes configuration
+app.use("/api/v1/auth", authRouter);
+
+// Not found Route
+app.use("/", () => {
+  throw createError.NotFound("Route not found...");
+});
+
+// Error handling middleware
+app.use(errorHandling);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
